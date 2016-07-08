15698c26be2bb58c84f9f07decc99a8c2da948e2
Keem.Net Inc <keem@keem.net>
2016-07-08 12:37:26 -0400

    Update for kth digit program
    Update

--- Lesson2/ReturnKthDigit.java
+++ Lesson2/ReturnKthDigit.java
@@ -6,7 +6,7 @@ public class ReturnKthDigit {
 
 	public static void main(String[] args) {
 		final int BASE = 10;
-		double x;
+		double x, x1;
 		int k;
 		
 		System.out.println("==============================================");
@@ -20,19 +20,31 @@ public class ReturnKthDigit {
 		x = inp.nextDouble();
 		System.out.print("Enter k:");
 		k = inp.nextInt();
-		
+		x1 = x;
 		if ( x < 0 ) {
 			x = -x;
-		}
-		int div = (int) Math.pow(BASE, k-1);
-		System.out.println(div);
-		if ( (div > 0) && (div < x)) {
-			int kthDigit = ((int)x/div) % 10;
-			//System.out.println(kthDigit);
-		} else if (div > x) {
-			System.out.println(k+" is greater than the maximum digit number");			
+		} else if (x < Integer.MAX_VALUE) {
+			int div = (int) Math.pow(BASE, k-1);
+			//System.out.println(div);
+			if ( (div > 0) && (div < x)) {
+				int kthDigit = ((int)x/div) % 10;
+				String nth = "th";
+				if( k == 1 ) nth = "st";
+				else if( k == 2 ) nth = "nd";
+				else if( k == 3 ) nth = "rd";
+				else if( k > 20) {
+					if( k%10 == 1 ) nth = "st";
+					else if( k%10 == 2 ) nth = "nd";
+					else if( k%10 == 3 ) nth = "rd";		
+				}
+				System.out.println("The "+k+nth+" digit of "+x1+" is "+kthDigit);
+			} else if (div > x) {
+				System.out.println(k+" is greater than the maximum digit number");			
+			} else {
+				System.out.println(k+" is not a valid digit number");
+			}			
 		} else {
-			System.out.println(k+" is not a valid digit number");
+			System.out.println("Input x is too big");
 		}
 	}
 
